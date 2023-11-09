df90a78899e0bd651743046d936666fe58f02c56
0-0snow0-0 <142613016+0-0snow0-0@users.noreply.github.com>
2023-11-09 14:35:30 -0500

    Update MainWindow.xaml.cs
    com. github

--- App1/App1/MainWindow.xaml.cs
+++ App1/App1/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace App1
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
+            //comantaire depuis github
             myButton.Content = "Clicked";
         }
     }
